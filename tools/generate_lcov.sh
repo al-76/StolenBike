bec1fc9bcdb2d7306cb5b744b17ec431152399d1
Viacheslav Konopkin <robotal76@gmail.com>
2023-02-18 18:49:02 +0300

    Delete tools directory

--- tools/generate_lcov.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-APP_NAME=StolenBike
-XCODE_DERIVED_DATA=~/Library/Developer/Xcode/DerivedData/$APP_NAME-*/Build
-PROFDATA=`readlink -f $XCODE_DERIVED_DATA/ProfileData/*/Coverage.profdata`
-COVERAGE=`readlink -f $XCODE_DERIVED_DATA/Products/Debug-iphonesimulator/$APP_NAME.app/$APP_NAME`
-
-xcrun llvm-cov export -format=lcov -instr-profile=$PROFDATA $COVERAGE > coverage.lcov
