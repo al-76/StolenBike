07b90cb99f1b50cb8288e483d5b62872efe42d2d
Vyacheslav Konopkin <robotal76@gmail.com>
2023-02-18 17:37:44 +0300

    Configure unit tests

---
+++ App/StolenBikeTests/EmptyTest.swift
@@ -0,0 +1,13 @@
+//
+//  EmptyTest.swift
+//  StolenBikeTests
+//
+//  Created by Vyacheslav Konopkin on 18.02.2023.
+//
+
+import XCTest
+
+final class EmptyTest: XCTestCase {
+    func testNothing() throws {
+    }
+}
